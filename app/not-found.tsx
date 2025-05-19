3904faaf94f9f643d7e0b3289a51625d7ba91341
Navneet-divine <navneetkush0102@gmail.com>
2025-05-19 19:50:33 +0530

    again 404 page updated

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,12 +4,14 @@ import Image from "next/image";
 
 export default function NotFound() {
   return (
-    <div className="flex gap-3 flex-col justify-center items-center h-screen">
+    <div className="flex gap-1 flex-col justify-center items-center h-screen">
       <div>
         <Image src={notFound} alt="notfoundimg" width={400} height={400} />
       </div>
-      <h2>Not Found</h2>
-      <Link href="/">Return Home</Link>
+      <h2 className="text-2xl font-montserrat">Not Found</h2>
+      <Link href="/" className="text-xl text-violet-500 underline">
+        Return Home
+      </Link>
     </div>
   );
 }
