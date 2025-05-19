7a970f4f48614d7799379f9e174f96cdd086bc40
Navneet-divine <navneetkush0102@gmail.com>
2025-05-19 19:16:15 +0530

    update the not-found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,10 +1,14 @@
 import Link from "next/link";
+import notFound from "@/public/images/not-found.svg";
+import Image from "next/image";
 
 export default function NotFound() {
   return (
-    <div className="flex justify-center items-center h-screen border border-red-400">
+    <div className="flex gap-3 flex-col justify-center items-center h-screen">
+      <div>
+        <Image src={notFound} alt="notfoundimg" width={100} height={100} />
+      </div>
       <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
       <Link href="/">Return Home</Link>
     </div>
   );
